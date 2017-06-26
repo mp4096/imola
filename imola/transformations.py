76ce49f0c909185e666ae7a29198c4494042058b
Mikhail Pak <mikhail.pak@tum.de>
2017-06-26 15:04:28 +0200

    Convert all inputs to ndarrays explicitly

--- imola/transformations.py
+++ imola/transformations.py
@@ -3,7 +3,10 @@ import numpy as np
 
 
 def inertial_to_body_frame(points, translation_vec, rotations):
+    points = np.array(points)
+    translation_vec = np.array(translation_vec)
     rotations = np.array(rotations)
+
     rot_mat_acc = np.eye(2)
     for phi in np.nditer(rotations):
         rot_mat_acc = _rotation_matrix(phi) @ rot_mat_acc
@@ -11,7 +14,10 @@ def inertial_to_body_frame(points, translation_vec, rotations):
 
 
 def body_to_inertial_frame(points, translation_vec, rotations):
+    points = np.array(points)
+    translation_vec = np.array(translation_vec)
     rotations = -np.array(rotations)
+
     rot_mat_acc = np.eye(2)
     for phi in np.nditer(rotations):
         rot_mat_acc = _rotation_matrix(phi) @ rot_mat_acc
