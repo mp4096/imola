d2ea10e543234d124acdde728d18c0c4221d4ecf
Mikhail Pak <mikhail.pak@tum.de>
2017-06-26 14:30:08 +0200

    Add geometric tranformations

---
+++ imola/transformations.py
@@ -0,0 +1,23 @@
+"""Functions for coordinate transformations."""
+import numpy as np
+
+
+def inertial_to_body_frame(points, translation_vec, rotations):
+    rotations = np.array(rotations)
+    rot_mat_acc = np.eye(2)
+    for phi in np.nditer(rotations):
+        rot_mat_acc = _rotation_matrix(phi) @ rot_mat_acc
+    return rot_mat_acc @ points + translation_vec
+
+
+def body_to_inertial_frame(points, translation_vec, rotations):
+    rotations = -np.array(rotations)
+    rot_mat_acc = np.eye(2)
+    for phi in np.nditer(rotations):
+        rot_mat_acc = _rotation_matrix(phi) @ rot_mat_acc
+    return rot_mat_acc @ (points - translation_vec)
+
+
+def _rotation_matrix(phi):
+    c, s = np.cos(phi), np.sin(phi)
+    return np.array([[c, -s], [s, c]])
