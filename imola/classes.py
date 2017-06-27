63b38b5f35c02843075166e4058422ceea90c852
Mikhail Pak <mikhail.pak@tum.de>
2017-06-27 21:03:43 +0200

    Add measurement noise class

--- imola/classes.py
+++ imola/classes.py
@@ -64,3 +64,22 @@ class EgoMotion:
         self.yaw_interp_der = np.array(
             interpolate.splev(self.u_interp, self.yaw_tck, der=1)
             )
+
+
+class MeasurementNoise():
+    def __init__(self, filename):
+        with codecs.open(filename, "r", encoding="utf-8") as f:
+            data = yaml.load(f)
+        # Select the measurement noise config only
+        cfg = data["measurement_noise"]
+
+        self.dof = cfg["radial"]["degrees_of_freedom"]
+        self.scaling = cfg["radial"]["scaling"]
+
+    def sample(self, num_samples):
+        # Draw the angle from the uniform pdf and
+        # the radius from the Student's t-distribution
+        rho = np.abs(np.random.standard_t(self.dof, size=num_samples))
+        rho *= self.scaling
+        phi = np.random.uniform(low=-np.pi, high=np.pi, size=num_samples)
+        return rho*np.vstack((np.cos(phi), np.sin(phi)))
